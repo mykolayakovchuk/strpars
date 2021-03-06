0489a5a9605c5100c000b2a32056c32e2005f3d5
Nikolay Yakovchuk <73442494+mykolayakovchuk@users.noreply.github.com>
2021-03-06 21:38:04 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 
 //Строка после разбиения на токены
-$tokens=["a","<","b",":","с",">","d","e"];
-
+//$tokens=["a","<","b",":","с",">","d","e"];
+$tokens=["d","e", ["f", "g"], ["h", "i"], "j"];
+echo  (count($tokens));
 //функция анализа верхушки стека
-function stackAnalize($array, $stackPointer){
+function stackProduce($array){
+    $stackPointer=count($array)-1;
     switch ($array[$stackPointer]) {
         case "<":
         case ":":
@@ -17,26 +19,77 @@ function stackAnalize($array, $stackPointer){
             if ($array[$stackPointer-1]!="<" &&
                 $array[$stackPointer-1]!=":" &&
                 $array[$stackPointer-1]!=">" &&
-                $array[$stackPointer-1]!="stackBottom"){
-                    //echo ("concat(".$array[$stackPointer]."+".($array[$stackPointer-1]).")<br>");
-                    break;
+                $array[$stackPointer-1]!="stackBottom"
+                ){
+                    //функция объединения значений
+                    $array[$stackPointer-1]=concat ($array[$stackPointer-1] , $array[$stackPointer]);
+                    array_pop($array);
             }else{
             break;}
     }
+return $array;
+}
+
+//функция объединения значений
+function concat ($element1, $element2){
+    switch (is_array($element1)){
+        case true:{
+            switch (is_array($element2)){
+                case true://оба элемента массивы (11)
+                    $resultArray=[];
+                    foreach($element1 as $value1){
+                        foreach($element2 as $value2){
+                            array_push($resultArray, $value1." ".$value2);
+                        }
+                    }
+                    return $resultArray;
+                    
+                    break;
+                case false://первый массив, второй не массив(10)
+                    $resultArray=[];
+                    foreach($element1 as $value){
+                        array_push($resultArray, $value." ".$element2);
+                    }
+                    return $resultArray;
+                    break;
+            }
+        }
+        case false:{
+            switch (is_array($element2)){
+                case true:// (01)
+                    $resultArray=[];
+                    foreach($element2 as $value){
+                        array_push($resultArray, $element1." ".$value);
+                    }
+                    return $resultArray;
+                    break;
+                case false: // (00)
+                    return $element1." ".$element2;
+                    break;
+            }
+        }
+
+    }
+   
 }
 
 //наполнение и работа со стеком
 $stack=["stackBottom"];
 $stackPointer=0;//указатель установлен на дно стека
 foreach ($tokens as $key=>$value){
+    echo ("<br>---0---------------------------<br>");
+    print_r($stack);
     array_push($stack, $value);
-    $stackPointer++;
-    stackAnalize($stack, $stackPointer);
+    $stack=stackProduce($stack);
+    echo ("<br>----1--------------------------<br>");
+    print_r($stack);
+    
 }
 //sdfsdfsdfgsdfg
+echo ("<br>-----tokens:--------------------<br>");
 print_r($tokens);
 
-echo ("<br>-------------------------------<br>");
+echo ("<br>-----stack:--------------------<br>");
 print_r($stack);
-echo ("<br>stackPointer=".$stackPointer);
+
 ?>
