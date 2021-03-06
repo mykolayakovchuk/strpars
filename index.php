4e502a25c7cf3a8ec068d1a9cf298b2763bb854f
Nikolay Yakovchuk <73442494+mykolayakovchuk@users.noreply.github.com>
2021-03-06 20:00:55 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,29 @@
 <?php
 
 //Строка после разбиения на токены
-$tokens=["a","<","b",":","с",">"];
+$tokens=["a","<","b",":","с",">","d","e"];
+
+//функция анализа верхушки стека
+function stackAnalize($array, $stackPointer){
+    switch ($array[$stackPointer]) {
+        case "<":
+        case ":":
+            //echo ":< detected<br>";
+            break;
+        case ">":
+            //echo "> detected<br>";
+            break;
+        default:
+            if ($array[$stackPointer-1]!="<" &&
+                $array[$stackPointer-1]!=":" &&
+                $array[$stackPointer-1]!=">" &&
+                $array[$stackPointer-1]!="stackBottom"){
+                    //echo ("concat(".$array[$stackPointer]."+".($array[$stackPointer-1]).")<br>");
+                    break;
+            }else{
+            break;}
+    }
+}
 
 //наполнение и работа со стеком
 $stack=["stackBottom"];
@@ -9,11 +31,12 @@ $stackPointer=0;//указатель установлен на дно стека
 foreach ($tokens as $key=>$value){
     array_push($stack, $value);
     $stackPointer++;
-
+    stackAnalize($stack, $stackPointer);
 }
 //sdfsdfsdfgsdfg
 print_r($tokens);
+
 echo ("<br>-------------------------------<br>");
 print_r($stack);
-echo ("<br>stackPointer=".$stackPointer)
+echo ("<br>stackPointer=".$stackPointer);
 ?>
